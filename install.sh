404227fcdb270cd9c46c96858ec6676f680c4de4
fireyang <shenhuan007@gmail.com>
2019-03-09 17:21:07 +0800

    add vim vim-plug

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "---------------------------------------------------------"
+echo "开始安装"
+echo "---------------------------------------------------------"
+
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  ~/.tmux/plugins/tpm/scripts/install_plugins.sh
+fi
+
+
+exit 0
