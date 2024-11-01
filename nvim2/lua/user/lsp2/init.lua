d6d5c18a35929abfd4d9c237732bcc3cfaa40869
fireyang <shenhuan007@gmail.com>
2024-11-01 13:01:52 +0800

    change nvim

--- nvim2/lua/user/lsp2/init.lua
+++ nvim2/lua/user/lsp2/init.lua
@@ -3,7 +3,7 @@ local mason_registry = require("mason-registry")
 local lspconfig = require("lspconfig")
 local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
 	.. "/node_modules/@vue/language-server"
-lspconfig.tsserver.setup({
+lspconfig.ts_ls.setup({
 	init_options = {
 		plugins = {
 			{
@@ -27,7 +27,7 @@ lspconfig.volar.setup({
 require("mason-lspconfig").setup_handlers({
 	function(server_name)
 		local server_config = {}
-		if server_name == "volar" or server_name == "tsserver" then
+		if server_name == "volar" or server_name == "ts_ls" then
 			return
 		end
 		lspconfig[server_name].setup(server_config)
